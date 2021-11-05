168645598f395214b221f6328e306715f598ace1
mmicheli <mmichelimendes@gmail.com>
2021-11-05 09:00:47 -0300

    integração chatbot

--- telegram.py
+++ telegram.py
@@ -1,5 +1,21 @@
 import telepot, json, time
+from telepot.loop import MessageLoop
 
-with open () as jsonFile: 
+with open ("token.json") as jsonFile: 
     token = json.load (jsonFile)
-    
+
+bot = telepot.Bot(token)
+
+def handle(msg):
+    content_type, chat_type, chat_id = telepot.glance(msg)
+
+    if content_type == 'text':
+        bot.sendMessage(chat_id, "Que bom te ver por aqui!")
+
+
+MessageLoop(bot, handle).run_as_thread()
+print ('Listening ...')
+
+# Keep the program running.
+while 1:
+    time.sleep(10)
